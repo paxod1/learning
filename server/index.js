da915d70dc61acd1ed6bcded4c36d76122bcd8b4
Jaya lakshmi <rjayalakshmi901@gmail.com>
2024-11-04 12:53:52 +0530

    adding vercel link to index.js file

--- server/index.js
+++ server/index.js
@@ -11,8 +11,9 @@ const port = 3000;
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173","https://learning-dashboard-client.vercel.app"],
     credentials:true,
+    methods:["GET","POST","PUT","DELETE"]
   }
   ));
 app.use(cookieParser());
