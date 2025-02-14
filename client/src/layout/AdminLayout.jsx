d0a894c47e7e760aadf75399612f11c8ad068ab2
chiku binoy <chikubinoybinoy@gmail.com>
2025-02-14 15:27:17 +0530

    hdfgng

--- client/src/layout/AdminLayout.jsx
+++ client/src/layout/AdminLayout.jsx
@@ -8,38 +8,35 @@ import { useDispatch, useSelector } from 'react-redux'
 import { clearUser, saveUser } from '../redux/features/userSlice'
 
 export const AdminLayout = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
     const location = useLocation();
     const dispatch = useDispatch();
-  
-    const { userAutherized } = useSelector((state) => state.user);
-  
+        const { userAutherized } = useSelector((state) => state.user);
+
     const checkUser = async () => {
-      try {
-        const response = await axiosInstance({ method: "GET", url: "/admins/check-admin" });
-        console.log(response, "====response");
-        dispatch(saveUser(response?.data?.data));
-      } catch (error) {
-        console.log(error?.response?.data, "===error");
-        dispatch(clearUser());
-      }
+        try {
+            const response = await axiosInstance({ method: "GET", url: "/admins/check-admin" });
+            console.log(response, "====response");
+            dispatch(saveUser(response?.data?.data));
+        } catch (error) {
+            console.log(error?.response?.data, "===error");
+            dispatch(clearUser());
+        }
     };
-  
+
     useEffect(() => {
-      checkUser();
+        checkUser();
     }, [location.pathname]);
-  
-    // Show <Header /> on login page
-    const isLoginPage = location.pathname === "/admin/login";
-  
-    return (
-      <div>
-        {isLoginPage ? <Header /> : userAutherized ? <AdminHeader /> : <Header />}
-  
+
+  return (
+    <div>
+       {userAutherized ? <AdminHeader /> : <Header />}
+
+        
         <div className='min-h-96'>
-          <Outlet />
+            <Outlet/>
         </div>
         <Footer />
-      </div>
-    );
-  };
+    </div>
+  )
+}
